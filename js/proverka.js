6d88dd9fe61b83433ffd829a23f7ac54b607737f
RadionKozachenko111 <radion.kozachenko21@gmail.com>
2021-07-06 21:37:04 +0300

    Main datat
    Main data

---
+++ js/proverka.js
@@ -0,0 +1,80 @@
+
+  new Swiper('.myswiper', 
+  { 
+
+    pagination: {
+    el:'.swiper-pagination',
+
+    clickable: true,
+    }, 
+
+    autoplay: {
+  
+    delay: 3000,
+    },
+
+    autoHeight: true,
+  
+    slidesPerView: 1,
+    
+    loop: true,
+
+  });
+// ! использую снипети jquery начала строки начинается с "jq"
+
+  function ibg(){
+
+    $.each($('.ibg'), function(index, val) {
+    if($(this).find('img').length>0){
+    $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+    }
+    });
+    }
+    
+    ibg();
+
+// ! для анимация скролинга по якорю
+// ! перенести в главний макет
+$(document).ready(function () {
+
+   $(".header__a").click(function (e) { 
+     e.preventDefault();
+     $("html,body").animate({
+      scrollTop: $($(this).attr("href")).offset().top+"px"
+     }, {
+       duration:1000,
+       easing:"linear",
+     });
+     return false;
+   });
+ });
+
+
+
+
+
+
+
+ // ! перенести в главний макет
+$(document).ready(function () {
+
+  $(".headertop__a").click(function (e) { 
+    e.preventDefault();
+    $("html,body").animate({
+     scrollTop: $($(this).attr("href")).offset().top+"px"
+    }, {
+      duration:1000,
+      easing:"linear",
+    });
+    return false;
+  });
+});
+
+
+
+$(document).ready(function() {
+  $('.burger-menu').click(function(event) {
+     $('.burger-menu,.header').toggleClass('active');
+     $('body').toggleClass('lock');
+  });
+  });
