1527e2067453567992e0883aca3bb749d6c74a99
RadionKozachenko111 <radion.kozachenko21@gmail.com>
2021-12-18 21:27:21 +0200

    add almost all script on native javascript

--- js/proverka.js
+++ js/proverka.js
@@ -1,86 +1,66 @@
-
-  new Swiper('.myswiper', 
-  { 
-
-    pagination: {
-    el:'.swiper-pagination',
+new Swiper(".myswiper", {
+  pagination: {
+    el: ".swiper-pagination",
 
     clickable: true,
-    }, 
+  },
 
-    autoplay: {
-  
+  autoplay: {
     delay: 3000,
-    },
+  },
 
-    autoHeight: true,
-  
-    slidesPerView: 1,
-    
-    loop: true,
+  autoHeight: true,
 
-  });
+  slidesPerView: 1,
+
+  loop: true,
+});
 // ! использую снипети jquery начала строки начинается с "jq"
 
-  function ibg(){
-
-    $.each($('.ibg'), function(index, val) {
-    if($(this).find('img').length>0){
-    $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+function ibg() {
+  let ibg = document.querySelectorAll(".ibg");
+  for (var i = 0; i < ibg.length; i++) {
+    if (ibg[i].querySelector("img")) {
+      ibg[i].style.backgroundImage =
+        "url(" + ibg[i].querySelector("img").getAttribute("src") + ")";
     }
-    });
-    }
-    
-    ibg();
+  }
+}
 
-// ! для анимация скролинга по якорю
-// ! перенести в главний макет
+ibg();
+
+// ! Переписать в скоре на native javaScript
 $(document).ready(function () {
-
-   $(".header__a").click(function (e) { 
-     e.preventDefault();
-     $("html,body").animate({
-      scrollTop: $($(this).attr("href")).offset().top+"px"
-     }, {
-       duration:1000,
-       easing:"linear",
-     });
-     return false;
-   });
- });
-
-
-
-
-
-
-
- // ! перенести в главний макет
-$(document).ready(function () {
-
-  $(".headertop__a").click(function (e) { 
+  $(".header__a").click(function (e) {
     e.preventDefault();
-    $("html,body").animate({
-     scrollTop: $($(this).attr("href")).offset().top+"px"
-    }, {
-      duration:1000,
-      easing:"linear",
-    });
+    $("html,body").animate(
+      {
+        scrollTop: $($(this).attr("href")).offset().top + "px",
+      },
+      {
+        duration: 1000,
+        easing: "linear",
+      }
+    );
     return false;
   });
 });
+// end
 
+let headeralla = document.querySelectorAll(".header__a");
+headeralla.forEach(function (item) {
+  item.addEventListener("click", function allHeader(event) {
+    event.preventDefault();
+    burgermenu.classList.remove("active");
+    document.querySelector(".header").classList.remove("active");
+  });
+});
 
-$(document).ready(function () {
-  $(".header__a").click(function (e) { 
-    e.preventDefault();
-    $(".burger-menu,.header").removeClass("active");
-  });
-}); 
-
-$(document).ready(function() {
-  $('.burger-menu').click(function(event) {
-     $('.burger-menu,.header').toggleClass('active');
-     $('body').toggleClass('lock');
-  });
-  });
+// burger Menu
+let burgermenu = document.querySelector(".burger-menu");
+burgermenu.addEventListener("click", function burgerMenu(event) {
+  event.preventDefault();
+  burgermenu.classList.toggle("active");
+  document.querySelector(".header").classList.toggle("active");
+  document.getElementsByTagName("body")[0].classList.toggle("lock");
+});
